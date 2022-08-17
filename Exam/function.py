4c663b1930b24677453d5445bf3e55ad8e9ebd87
Bernseng <kjp538@alumni.ku.dk>
2022-08-17 15:00:48 +0200

    added ledighed.csv

--- Exam/function.py
+++ Exam/function.py
@@ -86,9 +86,11 @@ def extract_UG(link):
     return main_text
 
 def clean_text(text):
+    text.split(' ')
     text = text.replace('[','').replace(']', '')
-    text = text.lower()
-    text = re.sub("(<li>)|(</li>)|</p>|<p>|\xa0|<ul>|</ul>|'|-| +|,| ,|</a>|<a>|<a|[.]|[:]", ' ', text)
+    text = text.str.lower()
+    text = re.sub("^\d+\s|\s\d+\s|\s\d+$", "", text)
+    text = re.sub("(<li>)|(</li>)|</p>|<p>|\xa0|<ul>|</ul>|'|-| +|,| ,|</a>|<a>|<a|[.]|[:]|\n", ' ', text)
     text = re.sub(" +", " ", text)
     return text
     
